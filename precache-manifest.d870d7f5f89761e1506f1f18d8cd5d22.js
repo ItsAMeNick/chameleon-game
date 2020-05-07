28be4ca2338f59fc3954d3307298540b27cec6dd
ItsAMeNick <zonern@rpi.edu>
2020-05-06 21:36:52 -0400

    Updates

---
+++ precache-manifest.d870d7f5f89761e1506f1f18d8cd5d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94da40522746bfb8b55ec543c43a8a99",
+    "url": "/chameleon-game/index.html"
+  },
+  {
+    "revision": "62302c143f3ce4d3f97c",
+    "url": "/chameleon-game/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "62302c143f3ce4d3f97c",
+    "url": "/chameleon-game/static/js/2.1531303d.chunk.js"
+  },
+  {
+    "revision": "3f3c6a0cfc36af7f7c565cabfd325de9",
+    "url": "/chameleon-game/static/js/2.1531303d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8f4d92d88724d0b9dd",
+    "url": "/chameleon-game/static/js/main.dec01b28.chunk.js"
+  },
+  {
+    "revision": "fc5e3d3308d8f8081604",
+    "url": "/chameleon-game/static/js/runtime-main.2c69b896.js"
+  }
+]);
